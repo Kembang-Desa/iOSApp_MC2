ad476c9c731278cf710720f0aae3cdd461ca648c
dithanrchy <dithanurcahya55@gmail.com>
2022-07-01 18:37:50 +0800

    add view dashboard

---
+++ iOSApp/PercentageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PercentageCollectionViewCell.swift
+//  iOSApp
+//
+//  Created by Ditha Nurcahya Avianty on 29/06/22.
+//
+
+import UIKit
+
+class PercentageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
