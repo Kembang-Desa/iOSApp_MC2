81e9d8331a9656749a1519f6c4071b162777913f
hastomimunthe <hastomi.riduan10@gmail.com>
2022-07-20 15:46:45 +0700

    ui  watch

---
+++ WatchAppItungin WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  WatchAppItungin WatchKit Extension
+//
+//  Created by Hastomi Riduan Munthe on 20/07/22.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+    @IBOutlet var speechButton: WKInterfaceButton!
+    
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
